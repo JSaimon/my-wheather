2c27c5c2536dc32340e202b124acd43e639443bb
JSaimon <komasich21@gmail.com>
2024-02-26 22:41:28 +0000

    typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "../App.css";
 export function Header() {
   return (
     <header>
-      <h1 className="title">myWheather</h1>
+      <h1 className="title">myWeather</h1>
     </header>
   );
 }
